ff7deded7ed655d74a11f7488bf59108b599e52c
abhi8893 <35069768+abhi8893@users.noreply.github.com>
2021-07-20 18:06:45 +0530

    implement LinearRegressionWithPValues model

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 import numpy as np
 import pandas as pd
 import scipy
@@ -39,3 +39,40 @@ class LogisticRegressionWithPValues(LogisticRegression):
         
 
         return summary_table
+
+
+class LinearRegressionWithPValues(LinearRegression):
+
+    def fit(self, X, y):
+        super().fit(X, y)
+
+        mse = ((self.predict(X) -  y)**2).sum()/(float(X.shape[0] - X.shape[1]))
+        std_err =  np.array([np.sqrt(np.diagonal(mse * np.linalg.inv(np.dot(X.T, X))))])
+        self.t_stat = self.coef_/std_err
+        tdist_degf = y.shape[0] - X.shape[1]
+        self.p_values = np.squeeze(2*( 1- scipy.stats.t.cdf(np.abs(self.t_stat), tdist_degf)))
+
+        try:
+            self.feature_names = X.columns
+        except AttributeError:
+            self.feature_names = [f'X{i}' for i in range(X.shape[1])]
+
+        return self
+
+    def get_summary_table(self, feature_names=None):
+
+        if feature_names is None:
+            feature_names = self.feature_names
+
+        # Same as above.
+        summary_table = pd.DataFrame(columns = ['feature_name'], data = feature_names)
+        summary_table['coefficient'] = np.transpose(self.coef_)
+        summary_table.index = summary_table.index + 1
+        summary_table.loc[0] = ['intercept', self.intercept_]
+        summary_table = summary_table.sort_index()
+        summary_table['p_value'] = np.append(np.nan, np.array(self.p_values))
+        
+        return summary_table
+
+
+    
